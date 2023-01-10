29bbf29193dea4e694c448afca050268edc0776a
Jéssica Florentino <jdgflorentino@gmail.com>
2023-01-10 11:40:25 -0300

    Updates

---
+++ precache-manifest.1d54e8fa911fe3e41a6fc1b7fec60d44.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f896e6b27fb00de995379dec855899",
+    "url": "/jdgflorentino/Frontend-Online-Store/index.html"
+  },
+  {
+    "revision": "dfa924b5a96e65d23c1d",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/css/2.2d6c900d.chunk.css"
+  },
+  {
+    "revision": "3f9d45c92ff1a0313395",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/css/main.7e0e3152.chunk.css"
+  },
+  {
+    "revision": "dfa924b5a96e65d23c1d",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/js/2.a633813e.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/js/2.a633813e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9d45c92ff1a0313395",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/js/main.70ccf129.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/js/main.70ccf129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b7b588378ea54161c5",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/js/runtime-main.6ebc9332.js"
+  },
+  {
+    "revision": "d8041e46c325f2414934a0b4633f945a",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/Compre.d8041e46.png"
+  },
+  {
+    "revision": "66a0be0f66b458b2578bc02ed3481711",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/Gamer.66a0be0f.png"
+  },
+  {
+    "revision": "3fa66bc435b6faa85341ddf12e805bbd",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/Gamer2.3fa66bc4.png"
+  },
+  {
+    "revision": "e79b5ff85f239697db59372c1aa729a4",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/Moda.e79b5ff8.png"
+  },
+  {
+    "revision": "525c3e4f3e736f23905e7f9fe2870cbb",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/mercado.525c3e4f.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/jdgflorentino/Frontend-Online-Store/static/media/slick.f97e3bbf.svg"
+  }
+]);
